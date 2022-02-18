5318d67d9ab9130fd92ba0e84a0275307deedde8
FranciscoAscue <francisco.ascue131@gmail.com>
2022-02-18 14:01:50 -0500

    update epical instalation

--- R/dependencies.R
+++ R/dependencies.R
@@ -5,15 +5,18 @@
 missingPackages <- function(pkg){
     if( !is.element(pkg,rownames(installed.packages() ) ) ){
       message(pkg, "-----> Package is not installed ")
+      if(pkg == "epical"){
+        remotes::install_github("chrismerkord/epical")
+      }
       install.packages(pkg)
     }
 }
 ################################################################################
 
 dependencies <- c("shiny","shinycssloaders","pheatmap","plotly","fossil",
-                  "epical","dplyr","rgdal","sp","sf","geojsonsf","DT",
+                  "remotes","dplyr","rgdal","sp","sf","geojsonsf","DT",
                   "leaflet","leaflet.minicharts","viridisLite","viridis",
-                  "RColorBrewer")
+                  "RColorBrewer","epical")
 
 ################################################################################
 # Package R dependencies
