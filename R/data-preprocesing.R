73dd89f6250a59336759cdacc7c9276248e57b6f
Orson <oamestanzam@unal.edu.co>
2022-03-21 10:06:28 -0500

    Update data-preprocesing.R

--- R/data-preprocesing.R
+++ R/data-preprocesing.R
@@ -128,7 +128,122 @@ freq_voc_voi <- function(data_1, lin){
 }
 
 
+split_lineages <- function(tabla,lineage1,gene,val){ # Main function
+  
+  #tabla$date1 <- epi_week_date(tabla$epi_week,tabla$epi_year,system = "cdc")
+  
+  selection <- filter(tabla, lineage == lineage1)
+  selection <- selection[,c("location","Substitutions","Date")] #selected columns for down analysis
+  
+  ## Function 1 add new column order and filter some gene
+  interest_mutation <- all_mutations(selection,gene) # search all mutations in spike protein. These function can be search any gen
+  
+  ## Function 2 selected uniq profile of mutations 
+  profiles <- uniq(interest_mutation,val,gene)
+  
+  ## Function 3 selected all mutations and return a dataframe in order 
+  tbl_resume <- do_table(profiles,gene)
+  
+  ## Function 4 search mutations and return the final table to show
+  result_table <- tbl_total(tbl_resume,profiles,gene)
+  
+  # aditional steps to do dataframe to input heatmap
+  newdata <- interest_mutation[ (interest_mutation$gen_select %in% profiles$gen_select), ]
+  heatmap_mutations <- newdata %>% count(newdata$gen_select,newdata$Date) 
+  names(heatmap_mutations) <- c("gene","epi_week","count") ## Here is possible return value for heatmap
+  
+  haplotype <- c()
+  
+  for (z in heatmap_mutations$gene){
+    haplo <- profiles[profiles$gen_select==z,]
+    haplotype <- append(haplotype,haplo$hap)
+  }
+  
+  heatmap_mutations$Profiles <- haplotype
+  heatmap_mutations$gene = str_replace_all(heatmap_mutations$gene,gene,"")
+  #return(heatmap_mutations)
+  return(list(table = result_table, heatmap = heatmap_mutations))
+}
 
+all_mutations <- function(mutations,gene) {  #Function 1
+  gen_muts <- c()
+  for (i in 1:nrow(mutations)){
+    static <- c()
+    nums <- c()
+    list_mutations <-strsplit(mutations$Substitutions[i], ",")
+    for (p in list_mutations[[1]]){
+      if(grepl(gene,p) == TRUE){
+        num <- stringr::str_extract(p, "\\d+")
+        nums <- append(nums,as.numeric(num))
+        static <- append(static,p)
+      } 
+    }
+    my_data <- data.frame(static, nums)
+    my_data <- my_data[order(my_data$nums),]
+    collapse <- paste(my_data$static, collapse = ",")
+    gen_muts <- append(gen_muts,collapse)
+    static <- c()
+    nums <- c()
+  }
+  mutations$gen_select <- gen_muts
+  return(mutations)
+}
+
+uniq <- function(interest_mutation,val,gene){ #Function 2
+  figure <- interest_mutation %>% group_by(gen_select) %>% summarize(count=n()) %>%  filter(count > val)
+  figure$hap <- sprintf("%03d", 1:nrow(figure))
+  return(figure)
+}
+
+do_table <- function(profiles,gene){
+  
+  new_vector <- c()
+  
+  profiles$gen_select = str_replace_all(profiles$gen_select,gene,"")
+  c <- strsplit(as.character(profiles$gen_select,gene),',')
+  for (i in 1:nrow(profiles)) {
+    new_vector <- append(new_vector,c[[i]])
+  }
+  
+  uniq_mut <- unique(new_vector)
+  
+  reference  <- c()
+  position <- c()
+  change <- c()
+  
+  for (y in uniq_mut){
+    
+    pos <- stringr::str_extract(y, "\\d+")
+    mut <- strsplit(y, split = pos)
+    
+    reference <- append(reference,mut[[1]][1])
+    position <- append(position,as.numeric(pos))
+    change <- append(change,mut[[1]][2])
+    
+  }
+  
+  table_1 <- data.frame(reference, change, position)
+  table_1 <- table_1[order(table_1$position),]
+  return(table_1)
+  
+}
+
+tbl_total <- function(tbl_resume,profiles,gene) {
+  
+  profiles$gen_select = str_replace_all(profiles$gen_select,gene,"")
+  for (y in 1:nrow(profiles)){
+    bools <- c()
+    for (i in 1:nrow(tbl_resume)){
+      
+      val <- paste0(tbl_resume$reference[i],tbl_resume$position[i],tbl_resume$change[i])
+      bool <- str_detect(profiles$gen_select[y],val)
+      bools <-append(bools,bool)
+    }
+    bools[isTRUE(bools)] <- 1
+    tbl_resume[paste0(profiles$hap[y])] <- bools
+  }
+  return(tbl_resume)
+}
 
 
 
